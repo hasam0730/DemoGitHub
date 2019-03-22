c8a3992d59f982278a63b753ff50a5e3cdfb7301
Hieu Nguyen <hieu.nguyen@2clicksolutions.com>
2019-03-22 23:47:26 +0700

    Initial Commit

---
+++ demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by Developer on 3/22/19.
+//  Copyright © 2019 2ClickSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
