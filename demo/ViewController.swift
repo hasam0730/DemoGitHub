a94f0acd4b988e42f7956c8873d3031c0db2654a
Hieu Nguyen <hieu.nguyen@2clicksolutions.com>
2019-03-23 17:04:55 +0700

    add new file and edit view controller file

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		textview.text = "Lorem ipsum dolor sit er Lorem ipsum dolor sit er"
 		textview.text = ""
 		textview.text = "Lorem ipsum dolor sit er"
 	}
