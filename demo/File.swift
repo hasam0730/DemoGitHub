cf3c208c5a2f5e7ad3c95b7608bef0c0f41a4158
Hieu Nguyen <hieu.nguyen@2clicksolutions.com>
2019-03-23 16:53:43 +0700

    add file and edit view controller

---
+++ demo/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  demo
+//
+//  Created by Developer on 3/23/19.
+//  Copyright © 2019 2ClickSolutions. All rights reserved.
+//
+
+import Foundation
--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		textview.text = ""
+		textview.text = "Lorem ipsum dolor sit er"
 	}
 }
 
